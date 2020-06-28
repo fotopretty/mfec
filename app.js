26266ab6f57aa59c3f09fd61d4359c9588bee5aa
Voravit Euavatanakorn <voravite@yahoo.com>
2020-06-28 13:41:58 +0700

    ปรับปรุง
    แล้วนะจ๊ะ

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+"use strict";
 const express = require('express');
-
 const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
 
-app.get('/', (req, res) => {
+const routes = require('./routes/index');
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(bodyParser.urlencoded({ extended: true }));
+// Set Parses JSON
+app.use(express.json());
+
+app.use(favicon(__dirname + "/favicon.ico"));
+app.use("/js", express.static("js"));
+app.use("/image", express.static("image"));
+app.use("/css", express.static("css"));
+
+app.use("/", routes);
+
+/* app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
 });
-
+ */
 
 
 // Start the server
